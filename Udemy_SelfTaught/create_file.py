03ed7e0f1e57950e8cb491cf27e84132d92baf99
ButtonWalker <dywayne.walker@gmail.com>
2020-11-09 11:47:52 -0600

    updates with new programs

--- Udemy_SelfTaught/create_file.py
+++ Udemy_SelfTaught/create_file.py
@@ -1,3 +1,46 @@
-st = ('st.txt', 'w')
-st.write('what up smart?')
-st.close()
+with open('st.txt', 'w') as f:
+    f.write('Sup suka!')
+    
+    
+my_list=list()
+
+with open('st.txt', 'r') as f:
+    my_list.append(f.read())
+    
+print(my_list)
+
+#write to CSV file
+
+import csv
+
+with open("st.csv", "w", newline='') as f:
+    write = csv.writer(f, delimiter=",")
+    write.writerow(["one",
+                    "two",
+                    "three"])
+    write.writerow(["four",
+                    "five",
+                    "six"])
+    
+    
+import csv
+
+with open('st.csv', 'r') as f:
+    r= csv.reader(f, delimiter=",")
+    for row in r:
+        print(','.join(row))
+        
+        
+import csv
+
+with open('movies.csv', 'w', newline='') as f:
+    write = csv.writer(f, delimiter=",")
+    write.writerow(['Top Gun',
+                    'Risky Business',
+                    'Minority Report'])
+    write.writerow(['Titanic',
+                    'The Revenant',
+                    'Inception'])
+    write.writerow(['Training Day',
+                    'Man on Fire',
+                    'Flight'])
