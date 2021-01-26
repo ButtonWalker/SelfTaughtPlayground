13419ddb83a95763bf0bb627f181e9389a2b0e7e
ButtonWalker <dywayne.walker@gmail.com>
2021-01-26 17:18:24 -0600

    new coding excersises

---
+++ CrashCourse/chpt2.py
@@ -0,0 +1,16 @@
+firstName = 'ada'
+lastName = 'lovelace'
+fullName = firstName + ' ' + lastName
+
+#print(fullName.upper())
+
+#print('Hello, ' + fullName.title() + '!')
+
+message = ('Hello, ' + fullName.title() + '!')
+print(message)
+
+print('Python')
+
+print('\tPython')
+
+print('Language:\nPython\nC\nJavaScript')
---
+++ CrashCourse/magicNumber.py
@@ -0,0 +1,6 @@
+answer = input ('Please guess a number?')
+
+if answer != 42:
+    print('That is not the currect answer. Please try again')
+else:
+    print('Your Right!!!')
